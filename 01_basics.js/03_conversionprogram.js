c2d87bf95c4fdceda5cddaa45887ce3d2f181cd5
abisek19 <shivayfurniture21@gmail.com>
2026-01-26 05:36:57 +0000

    conversion of datatypes

---
+++ 01_basics.js/03_conversionprogram.js
@@ -0,0 +1,18 @@
+let score = 0
+
+console.log(typeof (score))
+
+// ofc number hi aaya 
+
+let marks = "33abc"
+
+console.log(typeof marks)
+
+let ValueInNumber = Number(marks)
+
+console.log(typeof ValueInNumber )
+console.log(ValueInNumber)
+
+// true == 1
+// flase == 0
+// 33abc == NaN
