345b296a54d411268147b0f0f3cf1ffcb6215657
abisek19 <shivayfurniture21@gmail.com>
2026-01-27 14:11:34 +0000

    summary of datatypes

---
+++ summary-datatypes.js
@@ -0,0 +1,20 @@
+// 1. Primitive datatypes -
+
+const age = 23 // number 
+const kcal burned = 110.67 // number 
+
+let name = "Abisek." // string 
+
+const isLoggedIn = true // boolean 
+
+const outisdeTemp = null; // null 
+
+let userEmail; // undefined 
+
+const id = Symbol('123') // symbol 
+const anotherid = Symbol('123') // symbol 
+
+// 2. Non primitive datatypes 
+
+// Array , Objects , Functions 
+
