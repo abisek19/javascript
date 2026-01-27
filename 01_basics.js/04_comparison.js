2e34a38422fe38eeedcebb935b6887025a8eabdc
abisek19 <shivayfurniture21@gmail.com>
2026-01-27 13:34:43 +0000

    comparison of datatypes

---
+++ 01_basics.js/04_comparison.js
@@ -0,0 +1,19 @@
+// kya ho agar hum do alag data type elemenst ko compare kare for eg -
+console.log("2" == 2);
+
+
+// confusing ones -
+
+console.log(null == 0);
+console.log(null > 0);
+console.log(null >= 0);
+
+// yha pe teeno condition mai alg alg data types mai convert hua null and 0 isliye kisi mai false kisi mai true
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+// === triple means ye convert na karke sidha datatype hi check krta hai 
+
+console.log("2" > 2); // ab ye false aaya
