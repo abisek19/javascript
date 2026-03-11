1d0b5bdd380e7fc394174e03ae4b0729b0f4366e
abisek19 <shivayfurniture21@gmail.com>
2026-03-11 15:56:54 +0000

    huh

--- 02_basics.js/03_objects.js
+++ 02_basics.js/03_objects.js
@@ -1,8 +1,11 @@
 // object literals 
 
+const mysym = Symbol("key1")
+
 const myobj = {
     name : "Abisek",
     age: 18 ,
+   //mysym] : mykey1 ,
     email: "abhishek@google.com" ,
     isLoggedIn: false ,
     lastLoggedIn: ['Monday','sunday'] ,
@@ -10,3 +13,16 @@ const myobj = {
 }
 
 console.log(myobj.age)
+console.log(myobj["mother name"]) // isme aap direct . se nhi print kara sakte value 
+// ole.log(myobj[mysym])
+// symbol wali cheez imp hai 
+
+
+// freeze - is se aapke obj mai change hona bnd hojayega 
+
+myobj.email = "nankidevi@gmail.com "
+console.log(myobj.email)
+Object.freeze(myobj)
+myobj.email = "bhanuparatp@gmail.com"
+console.log(myobj.email)
+
