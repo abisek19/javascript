a65579165f7cb756af92551bb2090a6d00168518
abisek19 <shivayfurniture21@gmail.com>
2026-03-11 00:51:48 +0000

    arrays 2

---
+++ 02_basics.js/02_arrays.js
@@ -0,0 +1,15 @@
+const marvel_heros = ['thor','iron man','spider man']
+ 
+
+console.log(Array.isArray("Abisek."))
+console.log(Array.from("Abisek,"))
+
+
+let s1 = 100 
+let s2 = 200 
+let s3 = 300 
+
+console.log(Array.of(s1,s2,s3))
+
+
+ 
