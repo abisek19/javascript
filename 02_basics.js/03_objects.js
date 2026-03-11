114d0b7ff4c1ba3c767ab12b47b5f1cd59d41fe7
abisek19 <shivayfurniture21@gmail.com>
2026-03-11 01:18:47 +0000

    objects

---
+++ 02_basics.js/03_objects.js
@@ -0,0 +1,12 @@
+// object literals 
+
+const myobj = {
+    name : "Abisek",
+    age: 18 ,
+    email: "abhishek@google.com" ,
+    isLoggedIn: false ,
+    lastLoggedIn: ['Monday','sunday'] ,
+    "mother name": 'Aparajita'
+}
+
+console.log(myobj.age)
