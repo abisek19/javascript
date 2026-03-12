3c70fedfe3ffd844b9f6ed71f890ec1ab577187e
abisek19 <shivayfurniture21@gmail.com>
2026-03-12 01:28:56 +0000

    hufhbjkbn

---
+++ 02_basics.js/04_objects.js
@@ -0,0 +1,33 @@
+// single ton 
+const tinderUser = Object()
+
+tinderUser.email =" randuboy126@gmail.com "
+tinderUser.age = 18 
+tinderUser.name ="Alone boy Raju "
+
+console.log(tinderUser.name)
+
+const regularUser = {
+    Email :" panduranga@gmail.com " ,
+    Name : {
+        userFullName : {
+            firstname :'Raju',
+            lastname :'Dalal' 
+    }
+}
+}
+
+console.log(regularUser.Name.userFullName.firstname)
+
+const obj1 = {1 : 'a' , 2 : 'b'}
+const obj2 = {3 : 'a' , 4 : 'b' }
+const obj3 = {5 : 'a' , 6 : 'b'}
+
+const obj4 = Object.assign({} , obj1, obj2 ,obj3)
+console.log(obj4)
+
+// humne yha {} isliye lgaya taaki add hoke obj empty obj mai jaaye naaki first object mai 
+
+
+
+
