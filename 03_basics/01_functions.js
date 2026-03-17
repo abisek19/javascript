4066b35345c07c039251ff5636dae986b2c6b753
abisek19 <shivayfurniture21@gmail.com>
2026-03-17 13:59:31 +0000

    functions

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,19 @@
+function addTwoNumbers (num1 , num2){
+    result = num1 + num2
+    return result 
+    // iske baad kuch ni print hoga 
+}
+
+addTwoNumbers(3,4)
+console.log("Result:" , result); 
+
+function userLoggedIn (username){
+    if(!username){
+        console.log('Please enter your shit');
+
+        
+    } else 
+    return `${username} just gooned!`
+}
+
+console.log(userLoggedIn("Tejal"))
