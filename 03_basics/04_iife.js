888340daff1dc36a5c5c3965e191ce316914756d
abisek19 <shivayfurniture21@gmail.com>
2026-04-23 05:55:52 +0000

    iife

---
+++ 03_basics/04_iife.js
@@ -0,0 +1,17 @@
+// iife -
+
+// 1. nmaed iife - 
+
+(function chai() {
+    console.log("hello");
+    
+    
+})();
+
+// 2. arrow iife -
+
+( roti = () => {
+    console.log("bye");
+    
+
+})();
